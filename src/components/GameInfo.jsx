757d3c86ea24af91654902941c66f019c44448cf
GreenieGuest <papootreasure@yahoo.com>
2026-04-22 02:07:41 -0500

    feat: merge fixed in game info

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,30 +1,49 @@
 import { Container, VStack, Heading, Wrap, Center, WrapItem } from "@chakra-ui/react"
 
 const GameInfo = ({ state }) => {
-    return (
-        <Container>
-            <Wrap gap="5px" justify="center">
-            {state.teams.map((team, index) => (
-                <WrapItem key={index}>
-                    <Center padding="2px" w="200px" bg={state.teamInfo[index].color || "gray.400"} borderRadius="md" color={"black"}>
-                        <VStack>
-                            <Heading>{state.teamInfo[index].name}</Heading>
-                            <Wrap gap="5px" justify="center">
-                                {team.map((player, index) => (
-                                <WrapItem key={index}>
-                                    <Center padding="2px" bg={player.color || "gray.400"} borderRadius="md" color={"black"}>
-                                    {player.name}
-                                    </Center>
-                                </WrapItem>
-                                ))}
-                            </Wrap>
-                        </VStack>
-                    </Center>
-                </WrapItem>
-            ))}
-            </Wrap>
-        </Container>
-    )
+    if (state.quarter == 'M') {
+        return (
+            <Container>
+                <VStack>
+                    <Heading>Merge</Heading>
+                    <Wrap gap="5px" justify="center">
+                        {state.currentlyPlaying.map((player, index) => (
+                        <WrapItem key={index}>
+                            <Center padding="2px" bg={player.color || "gray.400"} borderRadius="md" color={"black"}>
+                            {player.name}
+                            </Center>
+                        </WrapItem>
+                        ))}
+                    </Wrap>
+                </VStack>
+            </Container>
+        )
+    } else {
+        return (
+            <Container>
+                <Wrap gap="5px" justify="center">
+                {state.teams.map((team, index) => (
+                    <WrapItem key={index}>
+                        <Center padding="2px" w="200px" bg={state.teamInfo[index].color || "gray.400"} borderRadius="md" color={"black"}>
+                            <VStack>
+                                <Heading>{state.teamInfo[index].name}</Heading>
+                                <Wrap gap="5px" justify="center">
+                                    {team.map((player, index) => (
+                                    <WrapItem key={index}>
+                                        <Center padding="2px" bg={player.color || "gray.400"} borderRadius="md" color={"black"}>
+                                        {player.name}
+                                        </Center>
+                                    </WrapItem>
+                                    ))}
+                                </Wrap>
+                            </VStack>
+                        </Center>
+                    </WrapItem>
+                ))}
+                </Wrap>
+            </Container>
+        )
+    }
 };
 
 export default GameInfo;
