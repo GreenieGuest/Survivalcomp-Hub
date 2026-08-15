4d5227eb048ca039fa91c24a905981bdb706689b
GreenieGuest <papootreasure@yahoo.com>
2026-08-15 01:37:37 -0500

    minor: default teams from The Lore

---
+++ src/constants/defaultTeams.js
@@ -0,0 +1,155 @@
+const default_teams = [
+    // BOTS 1: Red vs. Green vs. Blue
+    {
+        name: "Red Drums",
+        color: "#FF0000",
+    },
+    {
+        name: "Green Giants",
+        color: "#00FF00",
+    },
+    {
+        name: "Blue Jays",
+        color: "#0000FF",
+    },
+    {
+        name: "Team Electric",
+        color: "#ffff00",
+    },
+    {
+        name: "Team Stickbug",
+        color: "#7700ff",
+    },
+    // BOTS 2: In with the New
+    {
+        name: "Team Polar",
+        color: "#0088ff",
+    },
+    {
+        name: "Team Flame",
+        color: "#ff8800",
+    },
+    // BOTS 4: All Generations: 4-Team Phase
+    {
+        name: "The Conglomerate",
+        color: "#A17FFF",
+    },
+    {
+        name: "The Stickbug Confederacy",
+        color: "#FFE97F",
+    },
+    {
+        name: "The Coalition",
+        color: "#3F7F47",
+    },
+    {
+        name: "The Blue-Cyan Commonwealth",
+        color: "#7FC9FF",
+    },
+    // BOTS 10: All Stars
+    {
+        name: "Intricate Bastion",
+        color: "#cc0000",
+    },
+    {
+        name: "Rogue Horde",
+        color: "#dd7e6b",
+    },
+    {
+        name: "Sapphire Alliance",
+        color: "#1c4587",
+    },
+    {
+        name: "The Vindicators",
+        color: "#00FF00",
+    },
+    // BOTS 10 AS: 2 Teams Phase
+    {
+        name: "Carthaginian Crusaders",
+        color: "#7700ff",
+    },
+    {
+        name: "Legendary Legionnaires",
+        color: "#ff8800",
+    },
+    // 64DITP: The Ultimate Showdown (First Quarter)
+    {
+        name: "Dangerous Dynamites",
+        color: "#FF0000",
+    },
+    {
+        name: "Eclipsed Survivors",
+        color: "#00FF00",
+    },
+    {
+        name: "Raging Rapids",
+        color: "#0000FF",
+    },
+    {
+        name: "Tomamasi",
+        color: "#ffff00",
+    },
+    // 64DITP: The Ultimate Showdown (Second Quarter)
+    {
+        name: "Serene Sealions",
+        color: "#46bdc6",
+    },
+    {
+        name: "Orange Lionhearts",
+        color: "#ff9900",
+    },
+    {
+        name: "Fuchsia Rulers",
+        color: "#ff00ff",
+    },
+    // 64DITP: The Ultimate Showdown (Third Quarter)
+    {
+        name: "Hazardous Dukes",
+        color: "#cbff03",
+    },
+    {
+        name: "Midnight Lights",
+        color: "#9900ff",
+    },
+    // 64DITP2: The Postultimate Showdown (First Quarter)
+    {
+        name: "Fleeting Democracy",
+        color: "#00ffff",
+    },
+    {
+        name: "Arsenalites",
+        color: "#ff00ff",
+    },
+    {
+        name: "Rebellious Rattlesnakes",
+        color: "#ff9900",
+    },
+    // 64DITP2: The Postultimate Showdown (Second Quarter)
+    {
+        name: "Red Lumberjacks",
+        color: "#FF0000",
+    },
+    {
+        name: "Blue Pirates",
+        color: "#0000FF",
+    },
+    {
+        name: "Yellow Jackets",
+        color: "#ffff00",
+    },
+    // 64DITP2: The Postultimate Showdown (Third Quarter)
+    {
+        name: "Lavender Nobles",
+        color: "#2e00ba",
+    },
+    {
+        name: "Scarlet Crusaders",
+        color: "#ff4b01",
+    },
+    // Miscellaneous
+    {
+        name: "Teal Team",
+        color: "#00656a",
+    },
+]
+export default default_teams;
