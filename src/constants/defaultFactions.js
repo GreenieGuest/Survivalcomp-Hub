45a9cab78ac86a9e7bfd190e274cf3ea9d7ee315
GreenieGuest <papootreasure@yahoo.com>
2026-08-20 00:37:45 -0500

    minor: default faction names

---
+++ src/constants/defaultFactions.js
@@ -0,0 +1,36 @@
+const default_factions = [
+    // 64DITP
+    "Brigadiers",
+    "Strongman's Club",
+    "The Exiles",
+    "Sealions FC",
+    "Phoenix Alliance",
+    "The Renegadors",
+    "Intricate Bastion",
+    // 64DITP2
+    "Senators",
+    "Jocks",
+    "Adventure Alliance",
+    "Killer Alliance",
+    "The Outcasts",
+    "Scourge Alliance",
+    "The Resistance",
+    "Forensics Clique",
+    "Trifecta",
+    "The Brotherhood",
+    "Undertakers",
+    "Ingentium",
+    "CSI",
+    "Left-Behinders",
+    "Four Fighters",
+    "The Independents",
+    "New Senators",
+    "New Trifecta",
+    // Misc
+    "Rogue Horde",
+    "Keychainers",
+    "Toxic Four",
+    "Hunters' Alliance",
+    "Starburst Alliance"
+]
+export default default_factions;
